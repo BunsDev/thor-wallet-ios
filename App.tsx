15918efef79c763811d2a7a4d430b4bbcb058358
lucaspelloni2 <lucas_pelloni@hotmail.com>
2021-05-18 22:29:22 +0200

    adding react-native-navigation 5

--- App.tsx
+++ App.tsx
@@ -1,6 +1,16 @@
+import {NavigationContainer} from '@react-navigation/native';
+import {createStackNavigator} from '@react-navigation/stack';
 import React from 'react';
-import {View} from 'react-native';
+import {IntroSlider} from './src/components/screens/IntroSlider';
+
+const Stack = createStackNavigator();
 
 export const App = () => {
-  return <View />;
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={IntroSlider} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
 };
---
+++ src/components/screens/IntroSlider.tsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import {View} from 'react-native';
+
+export const IntroSlider = () => {
+  return <View style={{height: 400, width: 400, backgroundColor: 'red'}} />;
+};
