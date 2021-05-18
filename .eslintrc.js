84c6b67b8331eb9af9d8c5c9d519830436c76d56
lucaspelloni2 <lucas_pelloni@hotmail.com>
2021-05-18 21:52:45 +0200

    linting dependencies

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,15 @@
 module.exports = {
-  root: true,
-  extends: '@react-native-community',
+  extends: ['@jonny'],
+  rules: {
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    '@typescript-eslint/class-name-casing': 'off',
+    'fp/no-mutating-methods': 'off',
+    '10x/react-in-scope': 'error',
+    'ts-exports/unused-exports': 'off',
+    'default-param-last': 'off',
+    'react/boolean-prop-naming': 'off',
+  },
+  globals: {
+    __DEV__: true,
+  },
 };
