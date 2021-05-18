d35466b1e21f3898fa2b8c2bc58c011f049e4e00
lucaspelloni2 <lucas_pelloni@hotmail.com>
2021-05-18 22:22:02 +0200

    installing react-native-reanimated-2

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,6 @@
 module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
+  plugins: [
+    process.env.NODE_ENV === 'test' ? null : 'react-native-reanimated/plugin',
+  ].filter(Boolean),
 };
