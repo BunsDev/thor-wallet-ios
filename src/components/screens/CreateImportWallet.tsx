4423e7288d63ec3ed72deb467709278e44cd0c15
lucaspelloni2 <lucas_pelloni@hotmail.com>
2021-05-24 20:55:34 +0200

    wallet screen

--- src/components/screens/CreateImportWallet.tsx
+++ src/components/screens/CreateImportWallet.tsx
@@ -1,14 +1,26 @@
-import React from 'react';
+import LottieView from 'lottie-react-native';
+import React, {useEffect, useRef} from 'react';
 import {Button} from '../../ui/core/Button';
 import {Flex} from '../../ui/core/Flex';
+import {PressableLabel} from '../../ui/core/PressableLabel';
 import {MakeSpacing} from '../../ui/core/Spacer';
+import {Regular} from '../../ui/core/Typography';
 import {Background} from '../../ui/theme/Background';
 import {horizontalPadding, Padding} from '../../ui/theme/layout';
 import {BottomPadder} from '../BottomPadder';
 import {OnboardingHeading} from '../onboarding/OnboardingHeading';
 import {TopPadder} from '../TopPadder';
 
+const wallet = require('../../assets/animations/wallet.json');
+
+const END_FRAME = 39;
 export const CreateImportWallet = () => {
+  const ref = useRef<LottieView>(null);
+
+  useEffect(() => {
+    ref?.current?.play(0, END_FRAME);
+  }, []);
+
   return (
     <Background column flex={1}>
       <TopPadder />
@@ -18,7 +30,27 @@ export const CreateImportWallet = () => {
         subtitle={'Import or create your Wallet'}
       />
       <MakeSpacing yMultiply={8} />
-      <Flex flex={1} {...horizontalPadding} />
+      <Flex column flex={1} {...horizontalPadding}>
+        <Flex row justify="center">
+          <LottieView
+            ref={ref}
+            source={wallet}
+            autoPlay={false}
+            loop={false}
+            resizeMode="contain"
+            style={{
+              height: 160,
+              width: 160,
+            }}
+          />
+        </Flex>
+        <Regular center size="s">
+          You can either create a new wallet on the thorchain or import an
+          existing one. We can neither see nor transfer your private keys.{' '}
+        </Regular>
+        <MakeSpacing yMultiply={1} />
+        <PressableLabel label={'Learn more'} onPress={() => undefined} size="s" center />
+      </Flex>
       <Padding>
         <Button label={'Create Wallet'} />
         <Button label={'Import Wallet'} secondary />
--- src/ui/core/PressableLabel.tsx
+++ src/ui/core/PressableLabel.tsx
@@ -1,24 +1,26 @@
 import TouchableScale from '@jonny/touchable-scale';
 import React, {useCallback} from 'react';
 import {__COLORS} from '../theme/colors';
-import { Bold, Light, Regular, TypographySize } from "./Typography";
+import {Regular, TypographySize} from './Typography';
 
 type Props = {
   color?: string;
   size?: TypographySize;
+  center?: boolean;
   label: string;
   onPress: () => void;
 };
 export const PressableLabel = ({
   color = __COLORS.LEFT_GRADIENT,
   size = 'm',
-  label,
   onPress,
+  center,
+  label,
 }: Props) => {
   const press = useCallback(() => onPress(), [onPress]);
   return (
     <TouchableScale onPress={press}>
-      <Regular color={color} size={size}>
+      <Regular color={color} size={size} center={center}>
         {label}
       </Regular>
     </TouchableScale>
