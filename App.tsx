601a6d74153c3ac56a761f2465c7ee96e7bde687
lucaspelloni2 <lucas_pelloni@hotmail.com>
2021-05-24 13:34:26 +0200

    Update App.tsx

--- App.tsx
+++ App.tsx
@@ -1,6 +1,7 @@
 import {NavigationContainer} from '@react-navigation/native';
 import React from 'react';
 import 'react-native-gesture-handler';
+import {OverallManagers} from './src/components/OverallManagers';
 import {mainNavigator} from './src/navigation/DrawerActions';
 import {DrawerNavigator} from './src/navigation/DrawerNavigator';
 
@@ -8,6 +9,7 @@ export const App = () => {
   return (
     <NavigationContainer ref={mainNavigator}>
       <DrawerNavigator />
+      <OverallManagers />
     </NavigationContainer>
   );
 };
